34878a2d66b63f39d3d243545211650a5e10f83a
Phoudthavisack <66727427+Phoudthavisack@users.noreply.github.com>
2022-06-18 18:41:35 +0700

    commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const nocache = (_, resp, next) => {
 
 app.options("*", cors());
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "./build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
